49eb3a1ed82b77ad48dd1e562b2c0ff2c50ebdb6
unjoinable <135444615+unjoinable@users.noreply.github.com>
2025-06-28 17:57:40 +0530

    feat: everyone loves a teleporting sword

--- src/main/java/net/unjoinable/skyblock/item/ability/impls/InstantTransmission.java
+++ src/main/java/net/unjoinable/skyblock/item/ability/impls/InstantTransmission.java
@@ -2,6 +2,11 @@ package net.unjoinable.skyblock.item.ability.impls;
 
 import net.kyori.adventure.key.Key;
 import net.kyori.adventure.text.Component;
+import net.minestom.server.coordinate.Point;
+import net.minestom.server.coordinate.Pos;
+import net.minestom.server.coordinate.Vec;
+import net.minestom.server.instance.Instance;
+import net.minestom.server.utils.block.BlockIterator;
 import net.unjoinable.skyblock.item.SkyblockItem;
 import net.unjoinable.skyblock.item.ability.AbilityCostType;
 import net.unjoinable.skyblock.item.ability.ExecutionType;
@@ -15,6 +20,7 @@ import java.util.function.BiConsumer;
 
 public class InstantTransmission implements ItemAbility {
     private static final Key KEY = Key.key("ability:instant_transmission");
+    private static final Component INVALID_TELEPORT = MiniString.asComponent("<red>There are blocks in the way!</red>");
     private static final List<Component> DESCRIPTION = MiniString
             .listBuilder()
             .add("<gray>Teleport <green>8 blocks ahead</green> of you and")
@@ -58,9 +64,42 @@ public class InstantTransmission implements ItemAbility {
 
     @Override
     public BiConsumer<SkyblockPlayer, SkyblockItem> action() {
-        return (skyblockPlayer, _) -> {
-            // marker
-            skyblockPlayer.sendMessage("u kinda smell icl");
+        return (player,  _) -> {
+            Instance instance = player.getInstance();
+            Pos pos = player.getPosition();
+            Vec dir = getDirectionVector(pos.yaw(), pos.pitch());
+            BlockIterator iterator = new BlockIterator(pos.asVec(), dir, player.getEyeHeight(), 8);
+
+            Point teleportPos = pos;
+
+            while (iterator.hasNext()) {
+                Point point = iterator.next();
+
+                if (!instance.getBlock(point).isAir()) {
+                    player.sendMessage(INVALID_TELEPORT);
+                    break;
+                }
+                teleportPos = point;
+            }
+
+            player.teleport(Pos.fromPoint(teleportPos).withYaw(pos.yaw()).withPitch(pos.pitch()));
         };
+    }
+
+    /**
+     * Calculates a normalized direction vector from yaw and pitch angles
+     * @param yaw The horizontal rotation in degrees (0° = south, 90° = west, etc.)
+     * @param pitch The vertical rotation in degrees (0° = horizontal, -90° = up, +90° = down)
+     * @return A normalized Vec direction vector
+     */
+    private static Vec getDirectionVector(float yaw, float pitch) {
+        double yawRad = Math.toRadians(yaw);
+        double pitchRad = Math.toRadians(pitch);
+
+        double x = -Math.sin(yawRad) * Math.cos(pitchRad);
+        double y = -Math.sin(pitchRad);
+        double z = Math.cos(yawRad) * Math.cos(pitchRad);
+
+        return new Vec(x, y, z);
     }
 }
